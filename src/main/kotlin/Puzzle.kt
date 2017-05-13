de5db5cc79f1a825dbfc60d3ee929ad4d284bcb4
Imran Bohoran <imranbohoran@gmail.com>
2017-05-13 11:29:12 +0100

    Integrating the file choser and the Puzzle Solver

--- src/main/kotlin/Puzzle.kt
+++ src/main/kotlin/Puzzle.kt
@@ -1,9 +1,17 @@
+import model.PuzzleGrid
 import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 import javax.swing.JFileChooser
 
 fun main(args: Array<String>) {
     val chosenFile = chooseFile()
     println(chosenFile ?: "No file selected")
+    checkNotNull(chosenFile)
+    val fileContents = String(Files.readAllBytes(Paths.get(chosenFile?.toURI())))
+    val puzzleResult = PuzzleGrid(fileContents).solve()?.resultPath
+
+    println("The Path is: $puzzleResult")
 }
 
 fun chooseFile(): File? {
